c96ed04d8ae88ed86d4ce2c795e6bb41a17393e0
simao amaro <simaoamaro2000@gmail.com>
2022-06-13 00:48:29 +0100

    new

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent{
 
-  constructor(private elementRef: ElementRef) { }
+  constructor() { }
 
-  ngOnInit(): void {
-    
-  }
-  ngAfterViewInit() {
-    //this.elementRef.nativeElement.ownerDocument
-    //  .body.style.backgroundImage ="url('assets/Coding2.webp')"; 
-    this.elementRef.nativeElement.ownerDocument
-      .body.style.backgroundSize = "cover"; 
-  }
 
 }
