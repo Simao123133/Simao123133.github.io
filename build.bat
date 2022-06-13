bd748fd7d7fb6f73a9e273a9498fe7c39eebe992
simao amaro <simaoamaro2000@gmail.com>
2022-06-13 20:20:09 +0100

    working

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 CALL ng build --output-path docs --configuration production
 CALL copy docs\index.html docs\404.html
+CALL echo simao.amaro.sh  > docs\CNAME
 CALL git add .
 CALL git commit -m %1
 CALL git push --force
