2a16f9d4419d76ea0e3819f08e1a9bdfb531700d
simao amaro <simaoamaro2000@gmail.com>
2022-06-13 01:51:24 +0100

    new

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
-
+import { DeviceDetectorService } from 'ngx-device-detector';
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 })
 export class ResumeComponent implements OnInit {
 
-  constructor(private elementRef:ElementRef) { }
+  desktop: boolean = true;
+
+  constructor(private elementRef:ElementRef, private deviceService: DeviceDetectorService) { }
 
   ngOnInit(): void {
+    this.desktop = this.deviceService.isDesktop();
   }
 
   ngAfterViewInit() {
